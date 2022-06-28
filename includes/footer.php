6d4c04b78492dd4497c1cfd40d6cd25ba099b843
juel043 <juelhossain466@gmail.com>
2022-06-28 15:48:37 +0600

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,32 @@
+
+
+<footer>
+  <div class="footer-top">
+    <div class="container">
+      <div class="row">
+      
+        <div class="col-md-6">
+          <h6>About Us</h6>
+          <ul>
+
+        
+          <li><a href="">About Us </a></li>
+            <li><a href="">Privacy </a></li>
+          <li><a href="">Terms of use </a></li>
+               
+          </ul>
+        </div>
+  
+     
+      </div>
+    </div>
+  </div>
+  <div class="footer-bottom">
+    <div class="container">
+      <div class="row">
+          <p class="copy-right " style="text-align: center;">Copyright &copy; 2021  Car rantal Project </p>
+          <p class="copy-right " style="text-align: center;"> Develop By JRN Team </p>
+      </div>
+    </div>
+  </div>
+</footer>
---
+++ index.php
@@ -0,0 +1,119 @@
+
+<?php 
+session_start();
+include('includes/config.php');
+error_reporting(0);
+
+?>
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+
+<title>JRN Team</title>
+<link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css">
+<link rel="stylesheet" href="assets/css/style.css" type="text/css">
+<link rel="stylesheet" href="assets/css/owl.carousel.css" type="text/css">
+<link rel="stylesheet" href="assets/css/owl.transitions.css" type="text/css">
+<link href="assets/css/slick.css" rel="stylesheet">
+<link href="assets/css/bootstrap-slider.min.css" rel="stylesheet">
+<link href="assets/css/font-awesome.min.css" rel="stylesheet">
+
+
+<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet"> 
+</head>
+<body>
+
+       
+<?php include('includes/header.php');?>
+
+<section id="banner" class="banner-section" style="background-image: url('assets/images/us6.png');">
+  <div class="container">
+    <div class="div_zindex">
+      <div class="row">
+        <div class="col-md-5 col-md-push-7">
+          <div class="banner_content">
+            <h1>&nbsp;</h1>
+            <p>&nbsp; </p>
+            </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+ 
+<section class="section-padding gray-bg">
+  <div class="container">
+    <div class="section-header text-center">
+   
+      <p>Best Price with Quality Service</p>
+    </div>
+    <div class="row"> 
+      
+      <!-- Nav tabs -->
+      <div class="recent-tab">
+        <ul class="nav nav-tabs" role="tablist">
+          <li role="presentation" class="active"><a href="#resentnewcar" role="tab" data-toggle="tab">New Car</a></li>
+        </ul>
+      </div>
+      <!-- Recently Listed New Cars -->
+      <div class="tab-content">
+        <div role="tabpanel" class="tab-pane active" id="resentnewcar">
+
+<?php $sql = "SELECT tblvehicles.VehiclesTitle,tblbrands.BrandName,tblvehicles.PricePerDay,tblvehicles.FuelType,tblvehicles.ModelYear,tblvehicles.id,tblvehicles.SeatingCapacity,tblvehicles.VehiclesOverview,tblvehicles.Vimage1 from tblvehicles join tblbrands on tblbrands.id=tblvehicles.VehiclesBrand limit 9";
+$query = $dbh -> prepare($sql);
+$query->execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+$cnt=1;
+if($query->rowCount() > 0)
+{
+foreach($results as $result)
+{ 
+?>  
+
+<div class="col-list-3">
+<div class="recent-car-list">
+<div class="car-info-box"> <a href="vehical-details.php?vhid=<?php echo htmlentities($result->id);?>"><img src="admin/img/vehicleimages/<?php echo htmlentities($result->Vimage1);?>" class="img-responsive" alt="image"></a>
+<ul>
+<li><i class="fa fa-car" aria-hidden="true"></i><?php echo htmlentities($result->FuelType);?></li>
+<li><i class="fa fa-calendar" aria-hidden="true"></i><?php echo htmlentities($result->ModelYear);?> Model</li>
+<li><i class="fa fa-user" aria-hidden="true"></i><?php echo htmlentities($result->SeatingCapacity);?> seats</li>
+</ul>
+</div>
+<div class="car-title-m">
+<h6><a href="vehical-details.php?vhid=<?php echo htmlentities($result->id);?>"> <?php echo htmlentities($result->VehiclesTitle);?></a></h6>
+<span class="price">&#x09F3; <?php echo htmlentities($result->PricePerDay);?> /Day</span> 
+</div>
+<div class="inventory_info_m">
+<p><?php echo substr($result->VehiclesOverview,0,70);?></p>
+</div>
+</div>
+</div>
+<?php }}?>
+       
+      </div>
+    </div>
+  </div>
+</section>
+
+<?php include('includes/footer.php');?>
+
+<?php include('includes/login.php');?>
+
+<?php include('includes/registration.php');?>
+
+<?php include('includes/forgotpassword.php');?>
+
+
+
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script> 
+<script src="assets/js/interface.js"></script> 
+
+<script src="assets/js/bootstrap-slider.min.js"></script> 
+
+<script src="assets/js/slick.min.js"></script> 
+<script src="assets/js/owl.carousel.min.js"></script>
+
+</body>
+
+</html>
